676536f52f46316b47a62030c35ba6fa6b0879f3
re4dam <zakidam332@gmail.com>
2025-12-17 14:11:03 +0700

    routing web sudah ditangani

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\BarangController;
 Route::get('/', [BarangController::class, 'index'])->name('home');
 
 Route::get('alat/{id}', [BarangController::class, 'show'])->name('barang.show');
+
+Route::get('alat/{id}/keselamatan', [BarangController::class, 'panduanKeselamatan'])->name('barang.keselamatan');
+
+Route::get('alat/{id}/penggunaan', [BarangController::class, 'panduanPenggunaan'])->name('barang.panduan');
