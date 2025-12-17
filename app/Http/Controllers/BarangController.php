c852895673d9fd3e0a3a5c12bcf5bc5d223cd396
re4dam <zakidam332@gmail.com>
2025-12-17 14:11:46 +0700

    controller sudah terhubung dengan view home, detail, penggunaan, dan keselamatan

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -14,23 +14,38 @@ class BarangController extends Controller
         // Tidak meload tabel lain seperti komponen atau panduan
         $barang = Barang::all();
 
-        return view('barang.index', compact('barang'));
+        return view('home', compact('barang'));
     }
 
     public function show($id)
     {
         // Mengambil barang beserta tabel terkaitnya
         // Disini yaitu komponen, panduan keselamatan, dan panduan penggunaan
-        $barang = Barang::with(['komponen', 'panduanKeselamatan', 'panduanPenggunaan'])->findOrFail($id);
+        // $barang = Barang::with(['komponen', 'panduanKeselamatan', 'panduanPenggunaan'])->findOrFail($id);
+        $barang = Barang::with(['komponen'])->findOrFail($id);
         // findOrFail akan mengembalikan 404 jika data tidak ditemukan
 
         // mengembalikan dalam bentuk JSON
-        return $barang;
+        // return $barang;
 
         // adapun bentuk dump or die
         // dd($barang->toArray());
 
         // Tampilkan detail barang pada view
-        // return view('barang.show', compact('barang'));
+        return view('barang.detail', compact('barang'));
+    }
+
+    public function panduanKeselamatan($id)
+    {
+        $barang = Barang::with('panduanKeselamatan')->findOrFail($id);
+
+        return view('barang.panduankeselamatan', compact('barang'));
+    }
+
+    public function panduanPenggunaan($id)
+    {
+        $barang = Barang::with('panduanPenggunaan')->findOrFail($id);
+
+        return view('barang.panduan', compact('barang'));
     }
 }
